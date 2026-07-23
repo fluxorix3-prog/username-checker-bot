8801902ef0044af51a9f1a647b33349c5c1b166f
fluxorix3-prog <fluxorix3@gmail.com>
2026-07-23 08:32:05 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+SESSION_NAME = "checker"
